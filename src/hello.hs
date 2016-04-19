b448a308ad8efdf5fc94dea1af6f002ddd980108
unionx <unionx@foxmail.com>
2016-04-19 17:34:45 +0800

    what a mess

--- src/hello.hs
+++ src/hello.hs
@@ -1,8 +1,15 @@
 module Main where
 import System.Environment
-import Text.Pa
+import Text.ParserCombinators.Parsec hiding (spaces)
 
 main :: IO ()
--- what is 'referential transparency'?
 main = do args <- getArgs
           putStrLn ("Hello, " ++ args !! 0)
+
+symbol :: Parser Char
+symbol = oneOf "!$%&|*+-/:<=?>@^_~#"
+
+readExp :: String -> String
+readExp input = case parse symbol "lisp" input of
+    Left err -> "No match: " ++ show err
+    Right val -> "Found value"
